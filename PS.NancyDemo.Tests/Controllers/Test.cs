12344c83e0efa6e6b01729785f1ee93261b82359
sheva <stefan@codechem.com>
2020-05-28 17:16:48 +0200

    first commit

---
+++ PS.NancyDemo.Tests/Controllers/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PS.NancyDemo.Tests.Controllers
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
---
+++ PS.NancyDemo/HomeModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Nancy;
+using Nancy.Responses;
+
+namespace PS.NancyDemo
+{
+    public class HomeModule: NancyModule
+    {
+        public HomeModule()
+        {
+            Func<Request, bool> _isNotApiClient = req => !req.Headers.UserAgent.ToLower().StartsWith("curl");
+            
+            Get["/", ctx => _isNotApiClient.Invoke(ctx.Request)] = p => View["index.html"];
+
+            Get["/"] = p => "CURL func invoked";
+
+            Get["/courses"] = p => new JsonResponse(Course.List, new DefaultJsonSerializer());
+            Get["/courses/{id}"] = p => Response.AsJson(Course.List.SingleOrDefault(x => x.Id == p.id));
+        }
+    }
+}
